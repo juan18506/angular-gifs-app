666760ccb0218f309ba2e1fd4bbb6e0d73c04a1c
Juan Alves Pedreira <alvespedreiralucenojuanandres@gmail.com>
2023-10-25 18:41:27 -0300

    added localstorage support

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,10 +13,21 @@ export class GifsService {
   private apiKey: string = 'Tk50RfKNwN5HG7DHXgcIk0PPyyh6cxQX';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
-  get tagsHistory(): string[] {
-    return [...this._tagsHistory];
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   private organizeHistory(tag: string): void {
@@ -28,6 +39,7 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
   }
 
   searchTag(tag: string): void {
@@ -47,6 +59,11 @@ export class GifsService {
     // fetch('https://api.giphy.com/v1/gifs/search?api_key=Tk50RfKNwN5HG7DHXgcIk0PPyyh6cxQX&q=valorant&limit=10')
     //   .then(res => res.json())
     //   .then(data => console.log(data))
+
+  }
+
+  get tagsHistory(): string[] {
+    return [...this._tagsHistory];
   }
 
 }
