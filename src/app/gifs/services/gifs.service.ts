c0fbd18c9355b87821870f7998a18b0795cf5ab7
Juan Alves Pedreira <alvespedreiralucenojuanandres@gmail.com>
2023-10-24 22:19:44 -0300

    changed fetch API to angular httpClient

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,8 +7,10 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'Tk50RfKNwN5HG7DHXgcIk0PPyyh6cxQX';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
@@ -27,6 +30,20 @@ export class GifsService {
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http.get(`${this.serviceUrl}/search`, {params})
+      .subscribe(res => {
+        console.log(res);
+      });
+
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=Tk50RfKNwN5HG7DHXgcIk0PPyyh6cxQX&q=valorant&limit=10')
+    //   .then(res => res.json())
+    //   .then(data => console.log(data))
   }
 
 }
